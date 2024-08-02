8086b8e79e30766fb3fa376b8734a0ae3fc2ad21
GaetanoRe <gaetanore@live.com>
2024-08-02 13:33:07 -0400

    fixed movement to test.

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -12,7 +12,7 @@ func _process(delta):
 
 func _physics_process(delta):
 	
-	var direction = Input.get_vector("left", "right", "up", "down")
+	var direction = Input.get_vector("walk_left", "walk_right", "walk_up", "walk_down")
 	
 	velocity.x = direction.x * SPEED
 	velocity.y = direction.y * SPEED
