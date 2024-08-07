bef574fd0b7ca34ebd1d63c5f2b925326d5b8c73
GaetanoRe <gaetanore@live.com>
2024-08-07 13:58:29 -0400

    Made a great start for room generation

--- scripts/rooms/room_data.gd
+++ scripts/rooms/room_data.gd
@@ -3,11 +3,12 @@ extends Resource
 
 class_name RoomData
 
-enum room_type {Regular, Item, Passage}
+enum room_type {Regular, Item , Switch ,Passage}
 
 # Name of room
 @export var name : String
 @export var type : room_type
+@export var floor : int
 
 # References to rooms adjacent to this one
 @export var room_north : PackedScene
@@ -15,11 +16,17 @@ enum room_type {Regular, Item, Passage}
 @export var room_east : PackedScene
 @export var room_west : PackedScene
 
-# Room Specific Variables
-@export var room_pos : Vector2
-@export var room_length : float
-@export var room_width : float
+# Room Specific data
 
+# Item and Passage
+@export var has_item : bool
+@export var item_taken : bool
+
+# Switch
+@export var switch_activated : bool
+
+#Passage
+@export var boss_defeated : bool
 
 
 
--- scripts/world.gd
+++ scripts/world.gd
@@ -3,10 +3,31 @@ extends Node2D
 
 class_name World
 
-#func _ready():
-	#var player_scene = load("res://scenes/player/player.tscn")
-	#var player = player_scene.instantiate()
-	#add_child(player)
-	#var enemy_scene = load("res://scenes/enemy.tscn")
-	#var enemy = enemy_scene.instantiate()
-	#add_child(enemy)
+@export var curr_room : Room
+var player
+
+func _ready():
+	spawn_player()
+	curr_room = get_tree().get_first_node_in_group("room")
+	curr_room.entered_north.connect(change_room_scene_north)
+	curr_room.entered_south.connect(change_room_scene_south)
+	curr_room.entered_east.connect(change_room_scene_east)
+	curr_room.entered_west.connect(change_room_scene_west)
+
+
+func change_room_scene_north():
+	pass
+
+func change_room_scene_south():
+	pass
+
+func change_room_scene_east():
+	pass
+
+func change_room_scene_west():
+	pass
+
+func spawn_player():
+	var player_scene = load("res://scenes/player/player.tscn")
+	player = player_scene.instantiate()
+	add_child(player)
